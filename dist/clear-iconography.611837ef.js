9a82d3efb8c28257ec602d76a756c63680ec60ec
thiagodini <thiagocbdini@gmail.com>
2021-06-26 19:33:34 -0300

    Changes not commited

--- dist/clear-iconography.611837ef.js
+++ dist/clear-iconography.611837ef.js
@@ -117,7 +117,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+})({"../../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
@@ -149,7 +149,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],"../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+},{}],"../../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -184,12 +184,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/clear-iconography.css":[function(require,module,exports) {
+},{"./bundle-url":"../../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/clear-iconography.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./icons/ico.eot":[["ico.be0554e5.eot","styles/icons/ico.eot"],"styles/icons/ico.eot"],"./icons/ico.ttf":[["ico.84689a8b.ttf","styles/icons/ico.ttf"],"styles/icons/ico.ttf"],"./icons/ico.woff":[["ico.a7c16bd2.woff","styles/icons/ico.woff"],"styles/icons/ico.woff"],"./icons/ico.svg":[["ico.51eda07e.svg","styles/icons/ico.svg"],"styles/icons/ico.svg"],"_css_loader":"../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./icons/ico.eot":[["ico.be0554e5.eot","styles/icons/ico.eot"],"styles/icons/ico.eot"],"./icons/ico.ttf":[["ico.84689a8b.ttf","styles/icons/ico.ttf"],"styles/icons/ico.ttf"],"./icons/ico.woff":[["ico.a7c16bd2.woff","styles/icons/ico.woff"],"styles/icons/ico.woff"],"./icons/ico.svg":[["ico.51eda07e.svg","styles/icons/ico.svg"],"styles/icons/ico.svg"],"_css_loader":"../../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../../usr/local/share/.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34701" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35529" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
